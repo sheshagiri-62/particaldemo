d79a1b275d33b9f1c0863361f8db94f9fa616ab4
sheshagiri-62 <sheshagiri571@gmail.com>
2023-12-06 18:37:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from "react";
+import ReactDOM from "react-dom/client";
 
 function App() {
+  const [data,setData] = useState(
+    {
+      username:'',
+      password:''
+    }
+  );
+
+  const { username, password} =data
+  const changehandler = e => {
+    setData({...data,[e.target.name]:[e.target.value]})
+  }
+  const submithandler = e => {
+  e.preventDefault()
+  console.log(data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+        <form onSubmit= {submithandler}>
+          <h1> login form </h1>
+          <input type="text" name="username" value= {username}  onChange= {changehandler} /><br />
+          <input type="password" name="password" value= {password} onChange={changehandler} /><br />
+          <input type="submit" name="submit" /><br />
+        </form>
+      </center>
     </div>
   );
 }
